52dd4de0d8d2c3add7ce322db7fa2bd14ef91da8
Andrew <andrewinapple@gmail.com>
2018-10-10 18:38:22 +0300

    Доработки, исправления.

--- mainCartRender.js
+++ mainCartRender.js
@@ -1,9 +1,10 @@
 class MainCart {
     constructor(container = '#main-cart') {
         this.container = container;
-        this.source = JSON.parse(localStorage.getItem('mycart'));
-        this._render(this.source, this.container);
-        this.renderTotalCost();
+        this.cartItems = JSON.parse(localStorage.getItem('mycart'));// массив с объектами продуктов
+        this.amount = JSON.parse(localStorage.getItem('cartAmount'));
+        this._render(this.cartItems, this.container);
+        this._renderTotalCost();
     }
 
     /**
@@ -58,6 +59,7 @@ class MainCart {
             }).appendTo($rightPart);
 
             let $quantityInput = $('<input>', {
+                id: 'productQuantity',
                 type: 'text',
                 placeholder: `${product.quantity}`
             }).appendTo($quantitySpan);
@@ -74,19 +76,56 @@ class MainCart {
 
             let $removeBtn = $('<span>', {
                 class: 'product-characteristics'
-            }).appendTo($rightPart);
+            }).on('click', 'a', (event) => {
+                event.preventDefault();
+                this._removeProduct(event);
+                //console.log(event.target.getAttribute('data-id'));
+            })
+                .appendTo($rightPart);
 
             let $removeSign = $('<a>', {
+                'data-id': product.id_product,
                 class: 'button-all',
                 href: '#'
             }).appendTo($removeBtn);
 
-            let $x = $('<i class="fas fa-times"></i>').appendTo($removeSign);
+            let $x = $('<i class="fas fa-times"></i>')
+                .css('pointer-events', 'none')
+                .appendTo($removeSign);
         }
     }
 
-    renderTotalCost() {
-        let amount = localStorage.getItem('cartAmount');
+    _renderTotalCost() {
+        let amount = this.amount;
         $('.total-margin').text(`$${amount}`);
+    }
+
+    _updateQuantity(quantity, productWrapper) {
+        productWrapper.querySelector('#productQuantity').setAttribute('placeholder', `${quantity}`);
+    }
+
+    _removeProduct(event) {
+        for (let i = 0; i < this.cartItems.length; i++) {
+            //находим в cartItems[] соответствующий товар и работаем с ним в цикле
+            if (this.cartItems[i].id_product == event.target.getAttribute('data-id')) {
+                console.log('Hello');
+                this.cartItems[i].quantity--;
+                this.amount = this.amount - this.cartItems[i].price;
+                this.countGoods = this.countGoods - 1;
+                this._renderTotalCost(this.amount);
+                this._updateQuantity(this.cartItems[i].quantity, event.target.closest('.cart-content-container'));
+
+                if (this.cartItems[i].quantity < 1) {
+                    this.cartItems.splice(i, 1);
+                    //удление DOM элемента с товаром
+                    event.target.closest('.cart-content-container').remove();
+                } else {
+                    this._renderTotalCost(this.amount);
+                }
+                localStorage.setItem('mycart', JSON.stringify(this.cartItems));
+                localStorage.setItem('cartAmount', JSON.stringify(this.amount));
+            }
+        }
+
     }
 }
